22b4a37034547db9051b0c0693f3f5d72f5d9434
morteza mohammadi tabar <m.mohammaditabar64@gmail.com>
2024-04-10 23:45:29 +0330

    the last changes

--- drinks-app/src/app/app.component.ts
+++ drinks-app/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { Observable } from 'rxjs';
 import { ConfigServiceService } from './products/Service/config-service.service';
-import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
+
 })
 export class AppComponent {
   constructor(private configService: ConfigServiceService) { }
--- drinks-app/src/environments/environment.ts
+++ drinks-app/src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
     production: true,
+    apiServerAllProducts: 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic',
+    apiServerProduct: 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 };
